9f783507c82b636dd980fc2898a211dee7233ca9
KaviduAloka <aloka94@hotmail.com>
2022-04-07 08:56:34 +0530

    Inital commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# rn-auto-dimension-image
+
+This component can adjust the dimensions according to the given height or width. Both dimensions can be set for any local or server image
+
+## Installation
+`npm i rn-auto-dimension-image`
+
+## Usage
+
+```
+import React, {Component} from 'react';
+import {View} from 'react-native';
+
+import AutoDimensionImage from 'rn-auto-dimension-image';
+
+export default class Test extends Component {
+  render() {
+    return (
+      <View>
+        <AutoDimensionImage
+          uri="https://images.unsplash.com/photo-1616455579100-2ceaa4eb2d37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym13JTIwY2FyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+          dimensionType="width"
+          dimensionValue={300}
+          otherDimensionMaxValue={200}
+          style={{ alignSelf: 'center' }}
+        />
+
+        <AutoDimensionImage
+          dimensionType="height"
+          localSource={require('../image.png')}
+          dimensionValue={300}
+        />
+      </View>
+    );
+  }
+}
+```
+
+### Dimension change according to the input params (width & height)
+![Simulator Screen Recording - iPhone 12 - 2022-04-04 at 03 59 19](https://user-images.githubusercontent.com/31509440/161451497-55532ef8-717f-4c87-afc6-b43db90edba1.gif)
+
+
+### Adding `otherDimensionMaxValue`
+By adding this, we can add maximum limit to the dynamic dimension which gets changed according to the constant dynamic value (`dimensionValue`)
+
+![Simulator Screen Recording - iPhone 12 - 2022-04-04 at 04 01 07](https://user-images.githubusercontent.com/31509440/161451533-0b4864ab-9b81-4591-8fba-814d57d9a95a.gif)
+
+
+
+## Props
+
+| name | type | isRequired | default | description |
+| --- |  :---: |  :---: |  :---: |  --- |
+| dimensionType | `enum('width', 'height')` | YES | width | Mention the constant dimension. (width/height) |
+| dimensionValue | Number | YES | - | Constant dimension value |
+| uri | String | NO | - | URL of the server image |
+| localSource | `ImageSource` | NO | - | Local image path (`require('assets/image.jpg')`)
+| otherDimensionMaxValue | Number | NO | - | Can set the maximum value for the other dimension. If that value exceeds when setting the value for constant dimension, the image will fit to the given `otherDimensionMaxValue` value |
+| style | `Image Style Props` | NO | {} | Image styling | 
---
+++ src/index.js
@@ -0,0 +1,131 @@
+import PropTypes from 'prop-types';
+import React, {useState, useEffect} from 'react';
+import {Image, View, ActivityIndicator, StyleSheet} from 'react-native';
+
+const types = {
+  WIDTH: 'width',
+  HEIGHT: 'height',
+};
+const AutoDimensionImage = ({
+  uri,
+  localSource,
+  dimensionValue,
+  otherDimensionMaxValue,
+  dimensionType,
+  style,
+}) => {
+  const [loading, setLoading] = useState(true);
+  const [imageWidth, setImageWidth] = useState(0);
+  const [imageHeight, setImageHeight] = useState(0);
+
+  useEffect(() => {
+    setImageDimensions();
+  }, [dimensionValue, dimensionType, otherDimensionMaxValue]);
+
+  /**
+   * @returns {Promise}
+   */
+  const getImageDimensions = () => {
+    return new Promise((resolve, reject) => {
+      if (uri) {
+        //  server image
+        Image.getSize(
+          uri,
+          (width, height) => resolve({width, height}),
+          () => reject(null),
+        );
+      } else if (localSource) {
+        //  local image
+        const {width, height} = Image.resolveAssetSource(localSource);
+        resolve({width, height});
+      }
+    });
+  };
+
+  const setImageDimensions = () => {
+    getDynamicDimensions(dimensionType, dimensionValue)
+      .then(({width, height}) => {
+        const dynamicWidth = dimensionType === types.WIDTH;
+
+        if (!otherDimensionMaxValue) {
+          setImageWidth(width);
+          setImageHeight(height);
+        } else {
+          if (
+            (dynamicWidth && height < otherDimensionMaxValue) ||
+            (!dynamicWidth && width < otherDimensionMaxValue)
+          ) {
+            //  not exceeding the given otherDimensionMaxValue
+            setImageWidth(width);
+            setImageHeight(height);
+          } else {
+            //  exceeding the max value
+            getDynamicDimensions(
+              dimensionType === types.HEIGHT ? types.WIDTH : types.HEIGHT,
+              otherDimensionMaxValue,
+            )
+              .then(({width: _width, height: _height}) => {
+                setImageWidth(_width);
+                setImageHeight(_height);
+              })
+              .catch(error => console.log(`_ `, error));
+          }
+        }
+
+        setLoading(false);
+      })
+      .catch(error => console.log(error));
+  };
+
+  /**
+   * @returns {Promise}
+   */
+  const getDynamicDimensions = (type, value) => {
+    return new Promise((resolve, reject) => {
+      getImageDimensions()
+        .then(data => {
+          if (type === types.WIDTH) {
+            resolve({width: value, height: (data.height * value) / data.width});
+          } else {
+            resolve({width: (data.width * value) / data.height, height: value});
+          }
+        })
+        .catch(error =>
+          reject(`Could not get the image dimensions : ${error}`),
+        );
+    });
+  };
+
+  return (
+    <View>
+      {!loading ? (
+        <Image
+          source={uri ? {uri} : localSource}
+          style={[{width: imageWidth, height: imageHeight}, style]}
+        />
+      ) : (
+        <View style={styles.loadingWrapper}>
+          <ActivityIndicator />
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  loadingWrapper: {
+    padding: 10,
+    alignSelf: 'center',
+  },
+});
+
+AutoDimensionImage.propTypes = {
+  uri: PropTypes.string,
+  style: PropTypes.object,
+  dimensionValue: PropTypes.number.isRequired,
+  otherDimensionMaxValue: PropTypes.number,
+  dimensionType: PropTypes.oneOf(['width', 'height']).isRequired,
+  localSource: PropTypes.number,
+};
+
+export default AutoDimensionImage;
